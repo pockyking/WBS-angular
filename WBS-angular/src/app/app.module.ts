c6fbdb600ae1337f13eeba3db439c6346925c824
qqcom-cn <104134387+qqcom-cn@users.noreply.github.com>
2022-04-22 16:28:53 +0800

    ng-zorro-antd-mobile

--- WBS-angular/src/app/app.module.ts
+++ WBS-angular/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdMobileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
